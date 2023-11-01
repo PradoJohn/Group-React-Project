45d3cd70975f5591c57005fe7f1e5abf521bf284
pradobanz <pradobanz@gmail.com>
2023-11-01 12:26:02 -1000

    Test

--- our-app/src/Header.jsx
+++ our-app/src/Header.jsx
@@ -8,7 +8,7 @@ function Header(){
       <nav>
         <h1>ReactProject</h1>
         <ul>
-          <li><a href="/">Hangman</a></li>
+          <li><a href="/"><i class="fa fa-stop"></i></a></li>
           <li><a href="/mute-button">MuteButton</a></li>
           <li><a href="/pokemon">Pokemon</a></li>
           <li><a href="/to-do-list">ToDoList</a></li>
